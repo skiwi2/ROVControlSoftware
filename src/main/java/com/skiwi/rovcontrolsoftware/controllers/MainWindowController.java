c16c0d7fbb4eb4fd680e2086d96ff1d01b0da4dd
skiwi <frank_skiwi@hotmail.com>
2015-09-24 21:43:05 +0200

    Added basic implementation of sending commands via a socket.

--- src/main/java/com/skiwi/rovcontrolsoftware/controllers/MainWindowController.java
+++ src/main/java/com/skiwi/rovcontrolsoftware/controllers/MainWindowController.java
@@ -5,7 +5,6 @@ import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.ds.ipcam.IpCamDevice;
 import com.github.sarxos.webcam.ds.ipcam.IpCamDeviceRegistry;
 import com.github.sarxos.webcam.ds.ipcam.IpCamMode;
-import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
@@ -15,8 +14,15 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
+import java.net.Socket;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.ResourceBundle;
 
@@ -30,7 +36,13 @@ public class MainWindowController implements Initializable {
     private SwingNode swingNode;
 
     @FXML
-    private TextField urlTextField;
+    private TextField cameraUrlTextField;
+
+    @FXML
+    private TextField socketHostTextField;
+
+    @FXML
+    private TextField socketPortTextField;
 
     @FXML
     private Label xAngleLabel;
@@ -43,32 +55,76 @@ public class MainWindowController implements Initializable {
     private int xAngle;
     private int yAngle;
 
+    private Socket commandSocket;
+    private PrintWriter outCommandSocket;
+    private BufferedReader inCommandSocket;
+
+    private List<String> bufferedCommands = new ArrayList<>();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        urlTextField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+        cameraUrlTextField.textProperty().addListener((observableValue, oldValue, newValue) -> {
             //TODO unregistering causes a big problem if it is still in use
             IpCamDeviceRegistry.unregisterAll();
             try {
-                IpCamDeviceRegistry.register(new IpCamDevice("Test " + new Random().nextInt(), urlTextField.getText(), IpCamMode.PUSH));
+                IpCamDeviceRegistry.register(new IpCamDevice("Test " + new Random().nextInt(), cameraUrlTextField.getText(), IpCamMode.PUSH));
                 Platform.runLater(() -> swingNode.setContent(new WebcamPanel(Webcam.getDefault())));
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
         });
 
-        urlTextField.setText("http://195.235.198.107:3346/axis-cgi/mjpg/video.cgi");
+        socketHostTextField.textProperty().addListener((observableValue, oldValue, newValue) -> Platform.runLater(() -> updateSocketConnection()));
+        socketPortTextField.textProperty().addListener((observableValue, oldValue, newValue) -> Platform.runLater(() -> updateSocketConnection()));
+
+        cameraUrlTextField.setText("http://195.235.198.107:3346/axis-cgi/mjpg/video.cgi");
+        socketHostTextField.setText("127.0.0.1");
+        socketPortTextField.setText("2001");
         setXAngle(90);
         setYAngle(90);
+    }
+
+    private void updateSocketConnection() {
+        String host = socketHostTextField.getText();
+        String port = socketPortTextField.getText();
+
+        System.out.println("host = " + host);
+        System.out.println("port = " + port);
+
+        try {
+            if (commandSocket != null) {
+                commandSocket.close();
+            }
+
+            commandSocket = new Socket(host, Integer.parseInt(port));
+            outCommandSocket = new PrintWriter(commandSocket.getOutputStream(), true);
+            inCommandSocket = new BufferedReader(new InputStreamReader(commandSocket.getInputStream()));
+
+            bufferedCommands.forEach(outCommandSocket::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void setXAngle(int angle) {
         xAngle = angle;
         xAngleLabel.setText(String.valueOf(angle));
+        sendCommand("x " + angle);
     }
 
     private void setYAngle(int angle) {
         yAngle = angle;
         yAngleLabel.setText(String.valueOf(angle));
+        sendCommand("y " + angle);
+    }
+
+    private void sendCommand(String command) {
+        if (outCommandSocket == null) {
+            bufferedCommands.add(command);
+        }
+        else {
+            outCommandSocket.println(command);
+        }
     }
 
     public void setScene(Scene scene) {
