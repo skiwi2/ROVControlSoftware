a16705bec35740c28f66cc1b5d6470ed838f311f
skiwi <frank_skiwi@hotmail.com>
2015-09-24 18:00:08 +0200

    Added main window.

---
+++ src/main/java/com/skiwi/rovcontrolsoftware/ROVControlSoftwareApplication.java
@@ -0,0 +1,26 @@
+package com.skiwi.rovcontrolsoftware;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Frank van Heeswijk
+ */
+public class ROVControlSoftwareApplication extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("ROV Control Application");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
